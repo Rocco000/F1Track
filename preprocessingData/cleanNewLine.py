4eea8996045caf71b5dedac0727a439ab92b8aa0
sudo-poweroff <sdellaporta34@gmail.com>
2023-06-12 17:01:46 +0200

    Added data folders

--- preprocessingData/cleanNewLine.py
+++ preprocessingData/cleanNewLine.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
 def remove_field_from_rows(csv_file, columns, output_file):
-    df = pd.read_csv("./preprocessingData/"+csv_file)
+    df = pd.read_csv("./preprocessingData/originalData/"+csv_file)
     # Sostituisci \N con "" nelle colonne specificate
     df[columns] = df[columns].replace(r'\\N', '', regex=True)
-    df.to_csv("./preprocessingData/"+output_file, index=False)
+    df.to_csv("./preprocessingData/cleanedData/"+output_file, index=False)
 
 input_file = str(input("Inserisci il nome del file csv:\n"))
 output_file = str(input("Inserisci nome file di output:\n"))
