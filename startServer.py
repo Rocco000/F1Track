e19137a4ba7215f8c94cfecd7ed2573abdab47e0
Rocco000 <81473688+Rocco000@users.noreply.github.com>
2023-06-12 11:47:11 +0200

    Developed insert qualification and update results

--- startServer.py
+++ startServer.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, jsonify, redirect, url_for, s
 from pymongo import MongoClient
 from pymongo.errors import ConnectionFailure
 from datetime import datetime
+import pymongo
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'F1_Secret_Session_BD2'
@@ -52,7 +53,6 @@ def get_seasons():
 @app.route('/getDrivers', methods=['GET'])
 def get_drivers_season():
     season = int(request.args.get("year"))
-    print("Season: ",season)
     result = db['Races'].aggregate([
         {
             '$match':{
@@ -662,7 +662,11 @@ def admin_operation():
                 case 1:
                     return render_template("insert_form.html")
                 case 2:
-                    return render_template("update_form.html")
+                    result_season = db["Seasons"].find()
+                    seasons_list = list()
+                    for doc in result_season:
+                        seasons_list.append(doc["year"])
+                    return render_template("update_form.html", seasons=seasons_list)
                 case 3:
                     return render_template("delete_form.html")
                 case _:
@@ -683,6 +687,13 @@ def sort_insert():
                 return render_template("insert_race_page.html")
             case "circuit":
                 return render_template("insert_circuit_page.html")
+            case "qualification":
+                result = db["Seasons"].find()
+                result_list = list()
+                for doc in result:
+                    result_list.append(doc["year"])
+                return render_template("insert_qualification.html", s=result_list)
+
             case _:
                 return redirect(url_for("admin_home"))
     else:
@@ -698,6 +709,12 @@ def insert_driver():
         code = request.args.get("code")
         app_list = list((name, surname, birth, nat))
         if check_string(app_list):
+            #Check if the driver is already in the DB
+            result_check = db["Drivers"].find({"name":name, "surname":surname, "birthDate":birth})
+            result = list(result_check)
+            if len(result)>0:
+                flash(f"There is already a driver with this data!")
+                return redirect(url_for('admin_operation', operation="1"))
             max = get_max_field_value(db["Drivers"], "driverId") +1
             date_format = '%Y-%m-%d'
             date_object = datetime.strptime(birth, date_format)
@@ -727,6 +744,13 @@ def insert_circuit():
         alt = request.args.get("alt")
         app_list = list((name, city, country))
         if check_string(app_list):
+            #Check if the circuit is already in th DB
+            result_check = db["Circuits"].find({"name":name})
+            result = list(result_check)
+            if len(result)>0:
+                flash(f"There is already a circuit with this name!")
+                return redirect(url_for('admin_operation', operation="1"))
+
             max = get_max_field_value(db["Circuits"], "circuitId") +1
             insert_result = None
             if (lat is None or len(lat.strip())==0) and (long is None or len(long.strip())==0) and (alt is None or len(alt.strip())==0):
@@ -740,18 +764,22 @@ def insert_circuit():
                 insert_result = db["Circuits"].insert_one({"circuitId":max, "name":name, "city":city, "country":country, "position":{"longitude":long}})
             elif (lat is None or len(lat.strip())==0) and (long is None or len(long.strip())==0) and (not (alt is None or len(alt.strip())==0)):
                 #There is only altitude
+                alt = int(alt)
                 insert_result = db["Circuits"].insert_one({"circuitId":max, "name":name, "city":city, "country":country, "position":{"altitude":alt}})
             elif not((lat is None or len(lat.strip())==0) and (long is None or len(long.strip())==0)) and (alt is None or len(alt.strip())==0):
                 #There are latitude and longitude
                 insert_result = db["Circuits"].insert_one({"circuitId":max, "name":name, "city":city, "country":country, "position":{"latitude":lat, "longitude":long}})
             elif (lat is None or len(lat.strip())==0) and not((long is None or len(long.strip())==0) and (alt is None or len(alt.strip())==0)):
                 #There are longitude and altitude
+                alt = int(alt)
                 insert_result = db["Circuits"].insert_one({"circuitId":max, "name":name, "city":city, "country":country, "position":{"longitude":long, "altitude":alt}})
             elif (not (lat is None or len(lat.strip())==0)) and (long is None or len(long.strip())==0) and (not (alt is None or len(alt.strip())==0)):
                 #There are latitude and altitude
+                alt = int(alt)
                 insert_result = db["Circuits"].insert_one({"circuitId":max, "name":name, "city":city, "country":country, "position":{"latitude":lat, "altitude":alt}})
             else:
                 #There is a full position
+                alt = int(alt)
                 insert_result = db["Circuits"].insert_one({"circuitId":max, "name":name, "city":city, "country":country, "position":{"latitude":lat, "longitude":long, "altitude":alt}})
             
             if insert_result.acknowledged:
@@ -771,6 +799,13 @@ def insert_constructor():
         url=request.args.get("url")
         app_list=list((name,nationality))
         if check_string(app_list):
+            #Check if the constructor is already in the DB
+            result_check = db["Constructors"].find({"name":name})
+            result = list(result_check)
+            if len(result)>0:
+                flash(f"There is already a constructor with this name!")
+                return redirect(url_for('admin_operation', operation="1"))
+            
             max_id=get_max_field_value(db["Constructors"],"constructorId")+1
             insert_result=None
             if url is None or len(url.strip())==0:
@@ -787,6 +822,275 @@ def insert_constructor():
     else: 
         return redirect(url_for("home"))
 
+@app.route('/insertQualifyGetDriversRaces',methods=["GET"])
+def insert_qualify_get_driver():
+    if check_session():
+        season = request.args.get("year") 
+        if season is None or len(season.strip())==0:
+            flash(f"You missed the season!")
+            return redirect(url_for('admin_operation', operation="1"))
+        season = int(season)
+        result = db["Races"].find({"year":season})
+        first_document = next(result, None)
+        if first_document is not None:
+            #Season exists
+            races_name = list()
+            races_name.append({"id":first_document["raceId"], "name":first_document["name"]})
+
+            #Take all the races of the season
+            for doc in result:
+                races_name.append({"id":doc["raceId"], "name":doc["name"]})
+        
+            result_drivers = db["Drivers"].find() #Take all the drivers
+            result_constructor = db["Constructors"].find() #Take all the constructors
+            drivers_list = list()
+            constructors_list = list()
+            for doc in result_drivers:
+                drivers_list.append({"driverId":doc["driverId"], "name_surname":doc["name"]+" "+doc["surname"]})
+            for doc in result_constructor:
+                constructors_list.append({"constructorId":doc["constructorId"], "name":doc["name"]})
+                
+            return render_template("insert_qualification2.html", drivers=drivers_list, constructors=constructors_list, races=races_name)
+        else:
+            flash(f"The season is wrong!")
+            return redirect(url_for('admin_operation', operation="1"))
+    else: 
+        return redirect(url_for("home"))
+
+@app.route('/insertQualify',methods=["GET"])
+def insert_qualify():
+    if check_session():
+        raceId = request.args.get("raceName")
+        driverId = request.args.get("driver")
+        constructorId = request.args.get("constructor")
+        position = request.args.get("position")
+        q1 = request.args.get("q1")
+        q2 = request.args.get("q2")
+        q3 = request.args.get("q3")
+        app_list = list((driverId, constructorId, raceId, position, q1))
+        if check_string(app_list):
+            result_check = db["Qualifying"].find({"driverId":int(driverId), "raceId":int(raceId)})
+            result = list(result_check)
+            if len(result)>0:
+                flash(f"There is already a qualification round for this driver in this race!")
+                return redirect(url_for('admin_operation', operation="1"))
+            #User inserts the need fields
+            max_id=get_max_field_value(db["Qualifying"],"qualifyId")+1
+            insert_result = None
+            if not ( (q2 is None or len(q2.strip())==0)) and not((q3 is None or len(q3.strip())==0)):
+                #q2 and q3 is available
+                insert_result = db["Qualifying"].insert_one({"qualifyId":int(max_id), "raceId":int(raceId), "driverId":int(driverId), "constructorId":int(constructorId), "position":int(position), "q1":q1, "q2":q2, "q3":q3})
+            elif not ( (q2 is None or len(q2.strip())==0)) and (q3 is None or len(q3.strip())==0):
+                #q3 is not available
+                insert_result = db["Qualifying"].insert_one({"qualifyId":int(max_id), "raceId":int(raceId), "driverId":int(driverId), "constructorId":int(constructorId), "position":int(position), "q1":q1, "q2":q2})
+            elif (q2 is None or len(q2.strip())==0) and not ((q3 is None or len(q3.strip())==0)):
+                #q2 is not available but q3 yes -> error
+                flash(f"You send the third qualification time without the second qualification time!")
+                return redirect(url_for('admin_operation', operation="1"))
+            else:
+                #q2 and q3 are not available
+                insert_result = db["Qualifying"].insert_one({"qualifyId":int(max_id), "raceId":int(raceId), "driverId":int(driverId), "constructorId":int(constructorId), "position":int(position), "q1":q1})
+            
+            if insert_result.acknowledged:
+                flash(f"Added new qualification!")
+            else:
+                flash(f"Insert NOT done!")
+            return redirect(url_for('admin_operation', operation="1"))
+        else:
+            flash(f"You missed some data in the driver insert page!")
+            return redirect(url_for('admin_operation', operation="1"))
+    else:
+        return redirect(url_for("home"))
+
+@app.route('/sortUpdate', methods=["GET"])
+def sort_update():
+    if check_session():
+        collection = request.args.get("collection")
+        season = request.args.get("season")
+        app_list = list((collection, season))
+        if check_string(app_list):
+            #Check if the season is in the DB
+            check_query = db["Seasons"].find({"year":int(season)})
+            first_document = next(check_query, None)
+            if first_document is None:
+                flash(f"The season is wrong!")
+                return redirect(url_for('admin_operation', operation="2"))
+            
+            race_result = db["Races"].find({"year":int(season)}) #Take all races of a certain season
+            race_list = list()
+            for doc in race_result:
+                race_list.append({"raceId":doc["raceId"], "name":doc["name"]})
+
+            match collection:
+                case "result":
+                    return render_template("update_select_race.html", races=race_list, flag=1)
+                case "qualification":
+                    return render_template("update_select_race.html", races=race_list, flag=2)
+                case _:
+                    return redirect(url_for("admin_home"))
+        else:
+            flash(f"You missed data in the update section!")
+            return redirect(url_for('admin_operation', operation="2"))
+    else:
+        return redirect(url_for("home"))
+
+@app.route('/loadUpdateResult', methods=["GET"])
+def load_update_result():
+    if check_session():
+        raceId = request.args.get("race")
+        if raceId is not None and len(raceId.strip())!=0:
+            raceId = int(raceId)
+            #Check if the race exists in the DB
+            check_query = db["Results"].find({"raceId":raceId})
+            first_document = next(check_query, None)
+            if first_document is None:
+                flash(f"You sent a wrong race!")
+                return redirect(url_for('admin_operation', operation="2"))
+            
+            query_results = db["Results"].aggregate([
+                {
+                    '$match':{'raceId':raceId}
+                },
+                {
+                    '$lookup':{
+                        'from':'Drivers',
+                        'localField':'driverId',
+                        'foreignField':'driverId',
+                        'as':'drivers_result' 
+                    }
+                },
+                {
+                    '$unwind':'$drivers_result'
+                },
+                {
+                    '$project':{
+                        'driverId':'$drivers_result.driverId',
+                        'name':'$drivers_result.name',
+                        'surname':'$drivers_result.surname',
+                        'code':'$drivers_result.code',
+                        'constructorId':'$constructorId',
+                        'grid':'$grid',
+                        'positionText':'$positionText',
+                        'points':'$points',
+                        'laps':'$laps',
+                        'time':'$time',
+                        'fastestLap':'$fastestLap',
+                        'statusId':'$statusId'
+                    }
+                },
+                {
+                    '$lookup':{
+                        'from':'Constructors',
+                        'localField':'constructorId',
+                        'foreignField':'constructorId',
+                        'as':'constructors_result' 
+                    }
+                },
+                {
+                    '$unwind':'$constructors_result'
+                },
+                {
+                    '$project':{
+                        'driverId':'$driverId',
+                        'name':'$name',
+                        'surname':'$surname',
+                        'code':'$code',
+                        'constructor':'$constructors_result.name',
+                        'grid':'$grid',
+                        'positionText':'$positionText',
+                        'points':'$points',
+                        'laps':'$laps',
+                        'time':'$time',
+                        'fastestLap':'$fastestLap',
+                        'statusId':'$statusId'
+                    }
+                },
+                {
+                    '$lookup':{
+                        'from':'Status',
+                        'localField':'statusId',
+                        'foreignField':'statusId',
+                        'as':'status_result' 
+                    }
+                },
+                {
+                    '$unwind':'$status_result'
+                },
+                {
+                    '$project':{
+                        'driverId':'$driverId',
+                        'name':'$name',
+                        'surname':'$surname',
+                        'code':'$code',
+                        'constructor':'$constructor',
+                        'grid':'$grid',
+                        'positionText':'$positionText',
+                        'points':'$points',
+                        'laps':'$laps',
+                        'time':'$time',
+                        'fastestLap':'$fastestLap',
+                        'status':'$status_result.status'
+                    }
+                },
+                { '$sort':{'positionText':1}}
+            ])
+
+            drivers_result_list = list()
+            drivers_name_surname = list()
+            for doc in query_results:
+                str_app = doc["name"]+" "+doc["surname"]
+                drivers_name_surname.append({"driverId_raceId":str(doc["driverId"])+","+str(raceId), "name_surname":str_app})
+                drivers_result_list.append(doc)
+            
+            return render_template("update_result.html", result_race=drivers_result_list, drivers_name_surname=drivers_name_surname)
+        else:
+            flash(f"You missed the race data!")
+            return redirect(url_for('admin_operation', operation="2"))
+    else:
+        return redirect(url_for("home"))
+
+@app.route('/updateResult', methods=["GET"])
+def update_result():
+    if check_session():
+        driverId = request.args.get("driver")
+        points = request.args.get("points")
+        driverId, raceId = driverId.split(",")
+        app_list = list((driverId, raceId, points))
+        if check_string(app_list):
+            driverId = int(driverId)
+            raceId = int(raceId)
+            points = int(points)
+
+            #Check if the race exists in the DB
+            check_query = db["Results"].find({"raceId":raceId})
+            first_document = next(check_query, None)
+            if first_document is None:
+                flash(f"The race data are wrong!")
+                return redirect(url_for('admin_operation', operation="2"))
+            
+            #Check if the driver exists in the DB
+            check_query = db["Drivers"].find({"driverId":driverId})
+            first_document = next(check_query, None)
+            if first_document is None:
+                flash(f"The driver data are wrong!")
+                return redirect(url_for('admin_operation', operation="2"))  
+
+            #Update the result point for a certain driver
+            query = {"raceId":raceId, "driverId":driverId}
+            new_values = {"$set":{"points":points}}  
+            update_query = db["Results"].update_one(query, new_values)
+
+            #Check if the update operation is successful
+            if update_query.acknowledged and update_query.modified_count > 0:
+                return redirect(url_for('load_update_result', race=raceId))
+            else:
+                flash(f"Error in the update operation!")
+                return redirect(url_for('admin_operation', operation="2"))
+        else:
+            flash(f"You missed updated points or the driver data!")
+            return redirect(url_for('admin_operation', operation="2"))
+    else:
+        return redirect(url_for("home"))
 
 def check_session():
     if session:
